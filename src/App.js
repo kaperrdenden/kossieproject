9969237d1e2d51c398e07c72193972c0dab19fec
kaperrdenden <weather2sogood@gmail.com>
2022-01-22 00:17:31 +0900

    useEffect사용

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 function App() {
+  
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  const [username, setUsername] = useState('');
-  const [password,setPassword] = useState('');
+  useEffect(()=>{
+    console.log(count);
+  },[count,count2])
 
+  useEffect(()=>{
+    console.log('first rendering');
+  },[])
+  // 한번만 실행되고 그 이후에는 실행안됨
 
-  const onSubmit = (e)=>{
-    console.log('dsa')
-    e.preventDefault();
-  };
+  console.log("rendering");
+  // 이게 제일 먼저 console.log 찍힘
+  const increment = ()=>{
+    setCount(count+1);
+  }
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <input 
-          placeholder='username' 
-          value={username}
-          onChange={(e)=> setUsername(e.target.value)}
-        />
-        <br/>
-
-        <input 
-          placeholder='Password' 
-          value={password}
-          onChange={(e)=> setPassword(e.target.value)}
-        />
-        <br/>
-        
-      
-        <button onClick={onSubmit}>login</button>
-      </form>
+      <h1>kaperrdenden</h1>
+      {count}
+      <button onClick={increment}>Click</button>
+      <button onClick={()=>{setCount2(count2+1)}}>Click1</button>
     </div>
   );
 }
