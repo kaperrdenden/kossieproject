d287ee1be89f4324c0783acf72e0b3ec78c341d2
kaperrdenden <weather2sogood@gmail.com>
2022-01-22 00:01:49 +0900

    form창 내부의 input창 username, password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password,setPassword] = useState('');
+
+
+  const onSubmit = (e)=>{
+    console.log('dsa')
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input 
+          placeholder='username' 
+          value={username}
+          onChange={(e)=> setUsername(e.target.value)}
+        />
+        <br/>
+
+        <input 
+          placeholder='Password' 
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
+        />
+        <br/>
+        
+      
+        <button onClick={onSubmit}>login</button>
+      </form>
     </div>
   );
 }
