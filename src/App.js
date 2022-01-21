3311438b0c2183954c4797026801c8744aba692c
kaperrdenden <weather2sogood@gmail.com>
2022-01-22 01:31:17 +0900

    컴포넌트에 프롭스 보내기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import Counter from './components/Counter';
 function App() {
-  
-  // const [count, setCount] = useState(0);
-  // const [count1, setCount1] = useState(0);
-  // const [count2, setCount2] = useState(0);
-  
-  // const increment = ()=>{
-  //   setCount(count+1);
-  // }
-  // const increment1 = ()=>{
-  //   setCount1(count1+1);
-  // }
-  // const increment2 = ()=>{
-  //   setCount2(count1+2);
-  // }
+  const [buttonName, setButtonName] = useState('클릭');
+
+
   return (
     <div className="App">
       <h1>kaperrdenden</h1>
 
-    <Counter/>
-    <Counter/>
-    <Counter/>
+    <Counter click="click"/>
+    <Counter click={buttonName}/>
+    <Counter />
+    <button onClick={()=>{
+      setButtonName('click');
+    }}>click</button>
   
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 
 
-const Counter = () =>{
+const Counter = (props) =>{
     const [count, setCount] = useState(0)
     
     const increment = ()=>{
       setCount(count+1);
     }
- 
+    const clickString = props.click || 'Click';
+    // 만약 props.click이 있으면 해당 값이 clickString에 반환되고 없으면 'Click'이 반환된다
     return (
       <div className="App">
        
 
-        {count}<button onClick={increment}>Click</button>
+        {count}<button onClick={increment}>{clickString}</button>
   
         <br />
   
