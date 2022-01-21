45766d71ed02da78851c5488cec1d6b0eee0df8d
kaperrdenden <weather2sogood@gmail.com>
2022-01-22 03:56:23 +0900

    map사용해서 배열을 효율적으로 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import Counter from './components/Counter';
 function App() {
- 
-  
-  const [condition, setCondition] = useState(false);
-  
-  const toggle = ()=>{
-    setCondition(!condition);
-  }
-  useEffect(()=>{
-    console.log(condition);
-  },[condition])
 
-  const renderCondition = condition === true
-  ?' True'
-  :  'False'
 
+const movies = [
+  {title:'dwdqw odqwdq1', year: 2001,},
+  {title:'dwdqw odqwdq2', year: 2002,},
+  {title:'dwdqw odqwdq3', year: 2003,},
+  {title:'dwdqw odqwdq4', year: 2004,},
+]
+  
+const renderMovies = movies.map((movie,i)=>{
+  return (
+    <div className='movie' key={i}>
+    <div className='movie-title'>{movie.title}</div>
+    <div className='movie-year'>{movies.year}</div>
+  </div>
+  )
+})
   return (
     <div className="App">
-      <h1>kaperrdenden</h1>
-
-      <div>
-      {renderCondition}
-      </div>
-      <button onClick={toggle}>Toggle</button>
-    
-
+      <h1>Movie List</h1>
+    {renderMovies}
+ 
   
     </div>
   );
