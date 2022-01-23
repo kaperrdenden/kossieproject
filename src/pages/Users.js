0541fde6fce53c2af7105cdad8f8d59d964e6883
kaperrdenden <weather2sogood@gmail.com>
2022-01-24 00:35:34 +0900

    UserList만들어서 users데이터 컴포넌트화 하기

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,17 +1,24 @@
 import React from "react";
+import UserList from "../components/UserList";
 import axios from "axios";
-import { useEffect } from "react/cjs/react.development";
+import { useEffect, useState } from "react/cjs/react.development";
 
 const Users = ()=>{
+    const [users, setUsers] = useState([]);
+
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then( response =>{
-            console.log(response)
+           
+            setUsers(response.data);
         })
     },[]);
-
+    
     return(
-        <h1>Users</h1>
+        <>
+            <h1>Users</h1>
+            <UserList users={users}/>
+        </>
     )
 }
 
