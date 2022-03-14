f7af921bd011e5848c935dbb1295739a8ffc36a3
kaperrdenden <weather2sogood@gmail.com>
2022-03-15 05:00:05 +0900

    React에서 이벤트 핸들러

--- src/App.js
+++ src/App.js
@@ -11,21 +11,26 @@ import {
 
 function App() {
 
-
 // const [movieTitle,setMovieTitle] = useState('');
 // const [movieYear,setMovieYear] = useState('');
 // state를 따로 빼놓지 않고 관리했을 경우
 
+// return 내부에 있는 것들은 JSX. html, js도 아니다.        
 
-  
+  const onSubmit = ()=>{
+    alert('submitted');
+  }
+  const onKeyUp = (e)=>{
+    if(e.keyCode === 13){
 
-
+      alert('submitted');
+    }
+  }
   return (
-    <>
-    sdasd
-    
-    </>
-  
+    <div className='App'>
+      <input onKeyUp={onKeyUp}/>
+      <button onClick={onSubmit}>Submit</button>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // React.StrictMode는 렌더링은 되지않는다. 따라서 지워도 지장없다.
   document.getElementById('root')
 );
 
