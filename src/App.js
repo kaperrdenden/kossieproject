719eb0e3db1731521099459be718064828060d37
kaperrdenden <weather2sogood@gmail.com>
2022-03-16 01:37:44 +0900

    App.js에서 Count 스테이트와 로직짠걸 Counter 컴포넌트로 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-
+import Counter from './components/Counter';
 
 import {
     BrowserRouter as Router,
@@ -10,35 +10,23 @@ import {
 
 
 function App() {
-const [count, setCount] = useState(0);
-const [dong, setDong] = useState(0);
+
 // const [movieTitle,setMovieTitle] = useState('');
 // const [movieYear,setMovieYear] = useState('');
 // state를 따로 빼놓지 않고 관리했을 경우
-
 // return 내부에 있는 것들은 JSX. html, js도 아니다.        
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-  useEffect(()=>{
-    console.log("first");
-  },[])
   // const [username, setUsername] = useState('');
   // const [password, setPassword] = useState('');
   // const onSubmit = ()=>{
   //   alert('submitted');
   // }
- 
-const increment = () => {
-  setCount(count+1);
-}
+
   return (
     <div className='App'>
-      <button onClick={increment}>Click1</button>
-      <button onClick={() => {
-        setDong(dong+1);
-      }}>Click2</button>
+      <Counter />
+      <Counter />
+      <Counter />
+     
       {/* <form onSubmit={(e)=>{
         e.preventDefault();
       }}>
