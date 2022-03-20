66594b16262495ac43aae775918f97a8b1678cdd
kaperrdenden <weather2sogood@gmail.com>
2022-03-20 17:00:28 +0900

    Spinner 컴포넌트 만들고 axios요청이 끝나고 나면 spinner를 false로 만들어주기

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import UserList from "../UserList";
+import Spinner from "../Spinner";
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
 
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then(response => {
             setUsers(response.data);
-        })
+            setLoading(false);
+            // 로딩이 끝나면 false로 바꾸어 주는거
+        });
+
     },)
    
     return(
         <>
             <h1>users1</h1>
-            <UserList users={users} />
+           {loading
+            ? <Spinner />
+            :  <UserList users={users} />
+        }
+         
             </>
         )
 }
